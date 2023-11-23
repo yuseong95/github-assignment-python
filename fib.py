9f3cc3d069fcd9c777330914defbf10cf5f98eef
nys-2000 <nus9567@gmail.com>
2023-11-23 16:48:41 +0900

    Fix Failing FibonacciTestCases again

--- fib.py
+++ fib.py
@@ -1,16 +1,9 @@
-
-"""
-Fibonacci number generator
-When given a position, the function returns the fibonacci at that position in the sequence.
-The zeroth number in the fibonacci sequence is 0. The first number is 1
-Negative numbers should return None
-"""
 def fibonacci(position):
- if position < 0:
- return None
- elif position == 0:
- return 0
- elif position == 1:
- return 1
- else:
- return fibonacci(position - 1) + fibonacci(position - 2)
+    if position < 0:
+        return None
+    elif position == 0:
+        return 0
+    elif position == 1:
+        return 1
+    else:
+        return fibonacci(position - 1) + fibonacci(position - 2)
